a061cdd869a6dabd0f4ab67c59860e14bf98afd3
Hassan Khan <contact@hassankhan.me>
2016-02-17 17:45:21 +0000

    Included sample index.php Updated TODO.md

--- TODO.md
+++ TODO.md
@@ -1,8 +1,6 @@
 TODO
 
-- Add dotenv to require-dev
 - Add remaining entities
-- Check existing endpoints
 - Add tests
 - Fix pagination
 - Add more useful domain-specific exceptions
--- index.php
+++ index.php
@@ -19,48 +19,23 @@ $instagram = new \Instagram\Instagram(
 //dd($instagram);
 //$instagram->getUserByName('skrawg');
 header('Content-Type: application/json');
-echo json_encode($instagram->user()->getUserMedia('268047373')->get());
+//echo json_encode($instagram->user()->get('268047373')->get());
+//echo json_encode($instagram->user()->find('skrawg')->get());
+//echo json_encode($instagram->user()->getMedia('268047373')->get());
+//echo json_encode($instagram->user()->getLikedMedia()->get());
+//echo json_encode($instagram->user()->search('skrawg')->get());
 
-//dd($instagram->getProvider());
+//echo json_encode($instagram->media()->get('1109588739516340817_268047373')->get());
+//echo json_encode($instagram->media()->getByShortcode('9RV6okpRin')->get());
+//echo json_encode($instagram->media()->search(51.503349, -0.252271)->get());
 
-// $helper->getLoginUrl();
+if (!isset($_GET['code'])) {
 
-// if (!isset($_GET['code'])) {
+     // If we don't have an authorization code then get one
+     header('Location: '.$instagram->getLoginUrl());
+     exit;
 
-//     // If we don't have an authorization code then get one
-//     $authUrl = $client->getProvider()->getAuthorizationUrl();
-//     $_SESSION['oauth2state'] = $client->getProvider()->getState();
-//     header('Location: '.$authUrl);
-//     exit;
-
-// // Check given state against previously stored one to mitigate CSRF attack
-// } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-
-//     unset($_SESSION['oauth2state']);
-//     exit('Invalid state');
-
-// } else {
-
-//     // Try to get an access token (using the authorization code grant)
-//     $token = $client->getProvider()->getAccessToken('authorization_code', [
-//         'code' => $_GET['code']
-//     ]);
-
-//     // Optional: Now you have a token you can look up a users profile data
-//     try {
-
-//         // We got an access token, let's now get the user's details
-//         $user = $client->getProvider()->getResourceOwner($token);
-
-//         // Use these details to create a new profile
-//         printf('Hello %s!', $user->getName());
-
-//     } catch (Exception $e) {
-
-//         // Failed to get user details
-//         exit('Oh dear...');
-//     }
-
-//     // Use this to interact with an API on the users behalf
-//     echo json_encode($token->jsonSerialize());
-// }
+} else {
+     $token = $instagram->getAccessToken($_GET['code']);
+     echo json_encode($token);
+}

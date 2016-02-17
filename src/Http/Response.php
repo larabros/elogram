f1f6ffc2f9b62c272d0d804b720f6686ac6368b2
Hassan Khan <contact@hassankhan.me>
2016-02-17 17:42:42 +0000

    Cleaned up `Response` class

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -2,6 +2,14 @@
 
 namespace Instagram\Http;
 
+/**
+ * Response
+ *
+ * @package    Instagram
+ * @author     Hassan Khan <contact@hassankhan.me>
+ * @link       https://github.com/hassankhan/instagram-sdk
+ * @license    MIT
+ */
 class Response
 {
     protected $raw;
@@ -21,23 +29,50 @@ class Response
         $this->meta       = $meta;
         $this->data       = $data;
         $this->pagination = $pagination;
+        $this->raw        = [
+            'meta' => $this->meta,
+            'data' => $data,
+            'pagination' => $this->pagination
+        ];
     }
 
+    /**
+     * Creates a new instance of `Response` from a JSON-decoded response body.
+     *
+     * @param array $response
+     *
+     * @return static
+     */
     public static function createFromResponse(array $response)
     {
         return new static($response['meta'], $response['data'], $response['pagination'] ?: []);
     }
 
+    /**
+     * Returns JSON-decoded raw response.
+     *
+     * @return array
+     */
     public function getRaw()
     {
-        return ['meta' => $this->meta, 'data' => $data, 'pagination' => $this->pagination];
+        return $this->raw;
     }
 
+    /**
+     * Returns `data` from the response.
+     *
+     * @return array
+     */
     public function get()
     {
         return $this->data;
     }
 
+    /**
+     * Returns the next URL, if available, otherwise `null`.
+     *
+     * @return string|null
+     */
     public function next()
     {
         return array_key_exists('next_url', $this->pagination)
@@ -45,53 +80,13 @@ class Response
             : null;
     }
 
-//    public function paginate()
-//    {
-//        if (empty($this->pagination)) {
-//            throw new Exception();
-//        }
-//    }
-
-    public function toArray()
-    {
-        return $this->data;
-    }
-
+    /**
+     * Returns the JSON-encoded raw response.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return json_encode($this->getRaw());
     }
-
-    /**
-     * @inheritDoc
-     */
-//     public function paginate($response)
-//     {
-//         $responses   = [];
-//         $responses[] = $response;
-//         $pagination  = $this->toArray($response->pagination);
-//         $nextUrl     = str_replace(self::BASE_URL, '', $pagination['next_url']);
-
-//         do {
-//             $response    = $this->sendRequest('GET', $nextUrl);
-//             $responses[] = $response;
-//             $pagination  = $this->toArray($response->pagination);
-//             $nextUrl     = str_replace(self::BASE_URL, '', $pagination['next_url']);
-//         } while($pagination);
-// //        var_dump($pagination);
-
-//         return $responses;
-
-// //        do {
-// //
-// //        } while(true);
-
-// //        $nextUrlParams = [];
-// //        parse_str($pagination['next_url'], $nextUrlParams);
-//     }
-
-//     protected function checkForPagination($response)
-//     {
-
-//     }
 }

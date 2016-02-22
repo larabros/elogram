bfaba5216a36ead2107a0bcbb6eed36392af3eba
Hassan Khan <contact@hassankhan.me>
2016-02-22 00:49:57 +0000

    Added tests for `Config`

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -4,6 +4,7 @@ namespace Instagram\Tests\Http;
 
 use Instagram\Config;
 use Instagram\Tests\TestCase;
+use League\OAuth2\Client\Token\AccessToken;
 
 class ConfigTest extends TestCase
 {
@@ -11,16 +12,17 @@ class ConfigTest extends TestCase
 
     protected function setUp()
     {
-        $this->config = new Config();
+        $this->config = new Config([]);
     }
 
     public function testCreateWithoutAccessToken()
     {
-        $this->markTestIncomplete();
+        $this->assertNull($this->config->get('access_token'));
     }
 
     public function testCreateWithAccessToken()
     {
-        $this->markTestIncomplete();
+        $config = new Config(['access_token' => '{"access_token": "sometoken"}']);
+        $this->assertInstanceOf(AccessToken::class, $config->get('access_token'));
     }
 }

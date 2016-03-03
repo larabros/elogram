f10d86953145bbfadb4fc4f7d015971c76c719fb
Hassan Khan <contact@hassankhan.me>
2016-03-03 16:29:43 +0000

    Added `@covers` annotations to test class for `Config`

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -15,11 +15,21 @@ class ConfigTest extends TestCase
         $this->config = new Config([]);
     }
 
+    /**
+     * @covers Instagram\Config::__construct()
+     * @covers Instagram\Config::getDefaults()
+     * @covers Instagram\Config::get()
+     */
     public function testCreateWithoutAccessToken()
     {
         $this->assertNull($this->config->get('access_token'));
     }
 
+    /**
+     * @covers Instagram\Config::__construct()
+     * @covers Instagram\Config::getDefaults()
+     * @covers Instagram\Config::get()
+     */
     public function testCreateWithAccessToken()
     {
         $config = new Config(['access_token' => '{"access_token": "sometoken"}']);

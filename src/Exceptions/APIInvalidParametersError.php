b00ab32c61f9b826014e0ee72a93edd31c159bbf
Hassan Khan <contact@hassankhan.me>
2016-03-09 04:39:18 +0000

    Added a new exception class

---
+++ src/Exceptions/APIInvalidParametersError.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Larabros\Elogram\Exceptions;
+
+/**
+ * Thrown if a request has invalid or missing parameters.
+ *
+ * @package    Elogram
+ * @author     Hassan Khan <contact@hassankhan.me>
+ * @link       https://github.com/larabros/elogram
+ * @license    MIT
+ */
+class APIInvalidParametersError extends Exception
+{
+}

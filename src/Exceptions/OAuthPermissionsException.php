46574c228414509841c1c17f1bb273ae9ce3fd51
Hassan Khan <contact@hassankhan.me>
2016-03-10 02:55:37 +0000

    Added new exception class

---
+++ src/Exceptions/OAuthPermissionsException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Larabros\Elogram\Exceptions;
+
+/**
+ * Thrown if a request is made with an access token that is not authorized for
+ * a scope.
+ *
+ * @package    Elogram
+ * @author     Hassan Khan <contact@hassankhan.me>
+ * @link       https://github.com/larabros/elogram
+ * @license    MIT
+ */
+class OAuthPermissionsException extends Exception
+{
+}
